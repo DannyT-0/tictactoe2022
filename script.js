aa426ab850be9393620613bad1c7431eaeb6570b
danny <dannytokarev@gmail.com>
2022-09-13 10:42:57 -0600

    finished project. used old code to help write this program. Will come back to edit later

--- script.js
+++ script.js
@@ -1,27 +1,149 @@
-// create an array that will store the gameboard inside of the gameboard Object
-// Store players inside of an object as well
-// an object will also control the game flow as well
-// create the game flow and board with modules
-//create players with factory function
-// set up gameboard visually on screen
-//
+const container = document.querySelector(".boardContainer");
+const btnContainer = document.querySelector(".buttonContainer");
+const nameContainer = document.querySelector(".nameContainer");
+const playerOne = document.querySelector("#playerOne");
+const playerTwo = document.querySelector("#playerTwo");
+const startBtn = document.querySelector(".startGame");
+const resetBtn = document.querySelector(".resetGame");
+const rowBox = document.querySelectorAll(".rowBox");
+const results = document.querySelector(".announceResult");
 
-const gameBox = document.querySelectorAll(".gameBox");
+container.style.visibility = "hidden";
 
-const tokensInPlay = ["X", "O", "X", "O", "X", "O", "X", "O", "X"];
-const players = {};
-const gameFlow = {};
+alert("Please enter the player names below to start the game!");
 
-const playerFactory = (name, token) => {
-	const playerObj = () => name.push(players);
-	const tokenObj = () => token.push(players);
-	return { name, token };
+const players = (name, symbol) => {
+	return {
+		name,
+		symbol,
+	};
 };
 
-const danny = playerFactory("danny", "X");
+const gameBoard = (function () {
+	let xCount = 0;
+	let oCount = 0;
 
-gameBox.forEach((box) => {
-	box.addEventListener("click", () => {
-		box.innerText = "X";
+	const player1 = players(playerOne.value, "X");
+	const player2 = players(playerTwo.value, "O");
+
+	function addEvents() {
+		for (let i = 0; i < rowBox.length; i++) {
+			rowBox[i].addEventListener("click", (e) => {
+				if (xCount == 0 || xCount <= oCount || e.target.value == "X") {
+					e.target.textContent = player1.symbol;
+					e.target.value = e.target.textContent;
+					xCount++;
+				} else if (
+					(xCount > 0 && oCount == 0) ||
+					oCount < xCount ||
+					e.target.value == "O"
+				) {
+					e.target.textContent = player2.symbol;
+					e.target.value = e.target.textContent;
+					oCount++;
+				}
+				winConditions();
+			});
+		}
+	}
+
+	function winConditions() {
+		if (
+			(rowBox[0].textContent == "X" &&
+				rowBox[1].textContent == "X" &&
+				rowBox[2].textContent == "X") ||
+			(rowBox[3].textContent == "X" &&
+				rowBox[4].textContent == "X" &&
+				rowBox[5].textContent == "X") ||
+			(rowBox[6].textContent == "X" &&
+				rowBox[7].textContent == "X" &&
+				rowBox[8].textContent == "X") ||
+			(rowBox[0].textContent == "X" &&
+				rowBox[3].textContent == "X" &&
+				rowBox[6].textContent == "X") ||
+			(rowBox[1].textContent == "X" &&
+				rowBox[4].textContent == "X" &&
+				rowBox[7].textContent == "X") ||
+			(rowBox[2].textContent == "X" &&
+				rowBox[5].textContent == "X" &&
+				rowBox[8].textContent == "X") ||
+			(rowBox[0].textContent == "X" &&
+				rowBox[4].textContent == "X" &&
+				rowBox[8].textContent == "X") ||
+			(rowBox[2].textContent == "X" &&
+				rowBox[4].textContent == "X" &&
+				rowBox[6].textContent == "X")
+		) {
+			results.textContent = `${playerOne.value} wins!`;
+			xCount = 0;
+			oCount = 0;
+		} else if (
+			(rowBox[0].textContent == "O" &&
+				rowBox[1].textContent == "O" &&
+				rowBox[2].textContent == "O") ||
+			(rowBox[3].textContent == "O" &&
+				rowBox[4].textContent == "O" &&
+				rowBox[5].textContent == "O") ||
+			(rowBox[6].textContent == "O" &&
+				rowBox[7].textContent == "O" &&
+				rowBox[8].textContent == "O") ||
+			(rowBox[0].textContent == "O" &&
+				rowBox[3].textContent == "O" &&
+				rowBox[6].textContent == "O") ||
+			(rowBox[1].textContent == "O" &&
+				rowBox[4].textContent == "O" &&
+				rowBox[7].textContent == "O") ||
+			(rowBox[2].textContent == "O" &&
+				rowBox[5].textContent == "O" &&
+				rowBox[8].textContent == "O") ||
+			(rowBox[0].textContent == "O" &&
+				rowBox[4].textContent == "O" &&
+				rowBox[8].textContent == "O") ||
+			(rowBox[2].textContent == "O" &&
+				rowBox[4].textContent == "O" &&
+				rowBox[6].textContent == "O")
+		) {
+			results.textContent = `${playerTwo.value} wins!`;
+			xCount = 0;
+			oCount = 0;
+		} else if (xCount + oCount == 9) {
+			results.textContent = "It's a tie game! Please press reset";
+			xCount = 0;
+			oCount = 0;
+		}
+	}
+
+	return {
+		addEvents,
+	};
+})();
+
+gameBoard.addEvents();
+
+const gameControl = (function () {
+	let playBox = ["", "", "", "", "", "", "", "", ""];
+
+	for (let i = 0; i < rowBox.length; i++) {
+		playBox[i] == rowBox[i].value;
+	}
+
+	startBtn.addEventListener("click", () => {
+		if (
+			playerOne.value !== "" &&
+			playerTwo.value !== "" &&
+			container.style.visibility === "hidden"
+		) {
+			container.style.visibility = "visible";
+		}
 	});
-});
+
+	resetBtn.addEventListener("click", () => {
+		rowBox.forEach((box) => {
+			box.textContent = "";
+			box.value = "";
+			results.textContent = "";
+		});
+	});
+
+	return {};
+})();
